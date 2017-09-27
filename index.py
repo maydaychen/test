35dc0e18a576b0333106a1c68b138569605a6b31
chenyi <cy245832451@outlook.com>
2017-09-27 20:59:49 +0800

    9-27@property

--- index.py
+++ index.py
@@ -47,3 +47,45 @@ args = sys.argv
 print(args)
 print(init2("100"))
 print(int("100", 2))
+
+
+class Student(object):
+    def __init__(self, name, score):
+        self.name = name
+        self.score = score
+
+    def print_score(self):
+        print('%s: %s' % (self.name, self.score))
+
+
+asan = Student('asan', 59)
+
+print(asan.name)
+
+
+class Animal(object):
+    def run(self):
+        print("Animal is running")
+
+
+class Dog(Animal):
+    def run(self):
+        print("Dog is running")
+
+
+dog = Dog()
+dog.run()
+print(isinstance(dog, Animal))
+
+
+def run_twice(a):
+    a.run()
+    a.run()
+
+
+run_twice(Dog())
+
+print(type(123))
+print(type(dog))
+print(isinstance(123, (str, int)))
+print(dir(dog))
